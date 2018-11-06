473c7e78dbd9df0dd9add22577c27335a54f9167
nemkothari <nemkothari@gmail.com>
2018-11-06 12:49:30 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -6,6 +7,22 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 
+def rf_rfe(data) :
+    X = data.drop('SalePrice', axis=1)
+    Y = data['SalePrice']
+    #use linear regression as the model
+    lr = RandomForestClassifier()
+    #rank all features, i.e continue the elimination until the last one
+    lr.fit(X,Y)
+    rfe = RFE(lr, n_features_to_select=X.shape[1]/2 )
+    rfe.fit(X,Y)
+ 
+    li= zip(map(lambda x: round(x, 4), rfe.ranking_), X.columns.values)
+    ilist=[]
+    for i , j  in li:
+        if i ==1 :
+            ilist.append(j)
+    return ilist
 
-# Your solution code here
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -6,5 +7,22 @@ import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+def select_from_model(data) :
+    np.random.seed(9)
+    feature_name=[]
+    X = data.drop('SalePrice', axis=1)
+    Y = data['SalePrice']
+    lr = RandomForestClassifier()
+    #rank all features, i.e continue the elimination until the last one
+    #lr.fit(X,Y)
+    
+    sfm = SelectFromModel(lr)
+    sfm.fit(X, Y)
+ 
+    for feature_list_index in sfm.get_support(indices=True):
+        feature_name.append(X.columns.values[feature_list_index])
+    return feature_name
+        
+select_from_model(data)
 
-# Your solution code here
+
